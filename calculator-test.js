3f44726df29f17f39b7874786319034da4f1edab
bobadillamonica5 <120337175+bobadillamonica5@users.noreply.github.com>
2023-08-14 14:52:34 -0400

    Update object passed into Calc function

--- calculator-test.js
+++ calculator-test.js
@@ -3,7 +3,7 @@ it('should calculate the monthly rate correctly', function () {
   calculateMonthlyPayment({
     amount: 1000,
     years: 2,
-    rate: .25,
+    rate: .25
 }).toEqual(53.37);
 });
 
