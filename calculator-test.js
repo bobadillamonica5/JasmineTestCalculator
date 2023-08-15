f8eba7108b117cbde5775ddadb39bbb56480bdbf
Monica Bobadilla <monicabobadilla@monicabobadilla-macbookpro.local>
2023-08-15 19:50:30 -0400

    calc test

--- calculator-test.js
+++ calculator-test.js
@@ -1,10 +1,12 @@
 
 it('should calculate the monthly rate correctly', function () {
-  calculateMonthlyPayment({
-    amount: 1000,
-    years: 2,
-    rate: .25
-}).toEqual(53.37);
+  expect(
+    calculateMonthlyPayment({
+      amount: 1000,
+      years: 2,
+      rate: .25
+    })
+  ).toEqual('53.37');
 });
 
 
