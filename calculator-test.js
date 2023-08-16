7b026c3914b49ea98200ed5eb35afb6b24faea6c
Monica Bobadilla <monicabobadilla@monicabobadilla-macbookpro.local>
2023-08-15 20:25:29 -0400

    test html and test js

--- calculator-test.js
+++ calculator-test.js
@@ -10,8 +10,14 @@ it('should calculate the monthly rate correctly', function () {
 });
 
 
-// it("should return a result with 2 decimal places", function() {
-//   // ..
-// });
+it("should return a result with 2 decimal places", function() {
+  expect(
+    calculateMonthlyPayment({
+      amount: 1000,
+      years: 2,
+      rate: .25
+    })
+  ).toEqual('53.37');
+});
 
 // /// etc
